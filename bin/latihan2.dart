268693b7720e2a35d9e4d4c8b39bcfc9acc09fc5
ImmanuelTan <immanuel1828@gmail.com>
2022-05-29 15:14:58 +0700

    Materi Comment

---
+++ bin/latihan2.dart
@@ -0,0 +1,17 @@
+import 'package:latihan2/latihan2.dart' as latihan2;
+
+/// fungsi [main] akan menghasilkan 2 output
+/// Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada library [Belajar_Dart]
+void main(List<String> arguments) {
+  // Mencetak Hello Dart! Dart is great. pada konsol
+  print('Hello Dart! Dart is great.');
+  // Testing documentation comment with [].
+  print('6 * 7 = ${latihan2.calculate()}');
+}
+
+
+/*
+  output:
+    Hello Dart! Dart is great.
+    6 * 7 = 42
+ */
